489d7adb1437627456d8a4e4777ff774f0a567c1
index01d <indevelope@gmail.com>
2018-12-24 13:45:57 +0300

    [system] deploy

---
+++ pm2.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [{
+    name: 'tnShop',
+    script: './build/server.js',
+    cwd: '/home/thinknetica/client/current',
+    instances: 2,
+    exec_mode: 'cluster',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 9020
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'thinknetica',
+      host: ['tn.x01d.com'],
+      ref: 'origin/master',
+      repo: 'git.github.com:index01d/tnShop.git',
+      path: '/home/thinknetica/client',
+      'post-deploy': 'yarn && yarn build:server && pm2 startOrRestart pm2.config.js --env production'
+    }
+  }
+};
